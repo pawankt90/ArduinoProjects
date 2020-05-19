3177c7bc2d38ae548cd1ad3758d6a0c5cfb8b598
Pawan Tahiliani <pawantahiliani@gmail.com>
2020-05-19 02:11:30 -0700

    Pre-HP Swap

--- GemmaTest/GemmaTest.ino
+++ GemmaTest/GemmaTest.ino
@@ -2,13 +2,13 @@
 #include <Neopatterns.h>
 
 int GemmaLEDPin = 1;
-NeoPatterns neostrip = NeoPatterns(64, 6, GemmaLEDPin, NEO_GRB + NEO_KHZ800);
+NeoPatterns neostrip = NeoPatterns(64, 8, GemmaLEDPin, NEO_GRB + NEO_KHZ800);
 
 void setup() {
 	// put your setup code here, to run once:
 	neostrip.begin();
 	neostrip.show();
-	neostrip.ConfigureRainbowCycle(50);
+	neostrip.ConfigureRainbowCycle(25);
 }
 
 void loop() {
